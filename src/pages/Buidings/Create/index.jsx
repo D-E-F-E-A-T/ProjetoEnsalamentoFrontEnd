4d644f0a892dfd3e4da53c5ddd94934b7a7f2128
Henrique <henrique.rosa@meta.com.br>
2019-10-24 11:34:46 -0300

    adicionado checkbox

--- src/pages/Buidings/Create/index.jsx
+++ src/pages/Buidings/Create/index.jsx
@@ -1,101 +1,85 @@
-import React, { Component } from "react";
-import { Redirect } from "react-router-dom";
+import React, { useState, useEffect } from "react";
 import api from '../../../utils/API';
-import TextField from '@material-ui/core/TextField'
+import TextField from '@material-ui/core/TextField';
+import Checkbox from '@material-ui/core/Checkbox';
 
-class CreateBuilding extends Component {
-  constructor(props) {
-    super(props);
+const initialState = {
+  building: {
+    name: "",
+    status: true
+  },
+  erro: null
+}
 
-    this.state = {
-      building: {
-        name: "",
-        status: 1
-      },
-      erro: null,
-      redirect: false
-    };
-  }
+export default function CreateBuilding(props) {
+  const [state, setState] = useState(initialState);
 
-  exibeErro() {
-    const { erro } = this.state;
+  useEffect(() => {
 
-    if (erro) {
-      return (
-        <div className="alert alert-danger" role="alert">
-          Erro de conexão com o servidor
-        </div>
-      );
-    }
-  }
+  }, []);
 
-  render() {
-    const { redirect } = this.state;
-
-    if (redirect) {
-      return <Redirect to="/" />;
-    } else {
-      return (
-        <form onSubmit={(e) => {this.handleSubmit(); e.preventDefault();}}>
-          {this.exibeErro()}
-
-          <fieldset>
-            <legend>Create Building </legend>
-            <div className="form-group">
-              <label htmlFor="name">Name</label>
-              <TextField
-                type="text"
-                className="form-control"
-                id=""
-                name="name"
-                placeholder="name"
-                minLength="2"
-                maxLength="40"
-                value={this.state.building.name}
-                onChange={this.handleInputChange}
-              />
-            </div>
-            <div className="form-group">
-              <label htmlFor="status">Status</label>
-              <TextField
-                type="number"
-                className="form-control"
-                id="status"
-                name="status"
-                aria-describedby="statusAjuda"
-                placeholder="Status"
-                value={this.state.building.status}
-                onChange={this.handleInputChange}
-              />
-              </div>
-           
-            <button type="submit" className="btn btn-primary">
-              Send
-            </button>
-          </fieldset>
-        </form>
-      );
-    }
-  }
-
-  handleInputChange = event => {
+  const handleInputChange = event => {
     const target = event.target;
     const name = target.name;
     const value = target.value;
 
-    this.setState(prevState => ({
+    setState(prevState => ({
       building: { ...prevState.building, [name]: value }
+    }));
+  }
+
+  const handleChangeCheckbox = event => {
+    const name = event.target.name;
+    console.log(event)
+    setState(prevState => ({
+      building: { ...prevState.building, [name]: event.target.checked }
     }));
   };
 
-  handleSubmit = async() => {
-    api.post('/buildings', this.state.building)
+  const handleSubmit = async () => {
+    api.post('/buildings', state.building)
       .then(res => {
-        this.props.history.push('/buildings')
+        props.history.push('/buildings')
         console.log(res);
         console.log(res.data);
       })
   }
-}
 
-export default CreateBuilding;
+  return (
+    <form onSubmit={handleSubmit}>
+
+      <fieldset>
+        <legend>Create Building </legend>
+        <div className="form-group">
+          <label htmlFor="name">Name</label>
+          <TextField
+            type="text"
+            className="form-control"
+            placeholder="name"
+            minLength="2"
+            maxLength="40"
+            value={state.building.name}
+            onChange={handleInputChange}
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="status">Status</label>
+          <Checkbox
+            inputProps={{
+              'aria-label': 'primary checkbox',
+            }}
+            name="status"
+            value={state.building.status}
+            checked={state.building.status === true}
+            onChange={handleChangeCheckbox}
+          />
+        </div>
+
+        <button type="submit" className="btn btn-primary">
+          Send
+            </button>
+      </fieldset>
+    </form>
+  );
+
+};
--- src/pages/Buidings/Main/index.jsx
+++ src/pages/Buidings/Main/index.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
@@ -7,58 +7,61 @@ import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
 import Title from '../../../components/Navigation/Title';
 import api from '../../../utils/API';
+import { makeStyles } from '@material-ui/core';
 
-const useStyles = () => ({
+const useStyles = makeStyles({
     root: {
-      width: '100%',
-      overflowX: 'auto',
+        width: '100%',
+        overflowX: 'auto',
     },
     table: {
-      minWidth: 650,
+        minWidth: 650,
     },
-  });
+});
 
-export default class MainUsuario extends Component {
-    state = {
-        building: [],
-        erro: null
-    }
+const initialState = {
+    building: [],
+    erro: null
+}
 
-    async componentDidMount() {
+export default function MainUsuario() {
+    const [state, setState] = useState(initialState);
+    const classes = useStyles();
+    const { building } = state;
 
-        const response = await api.get(`/Buildings`);
-        
-        this.setState({ building: response.data });
+    useEffect(() => {
+        async function getBuildings() {
+            const response = await api.get(`/Buildings`);
 
-    }
+            setState({ building: response.data });
+        }
 
-    render() {
-        const classes = useStyles();
-        const { building } = this.state;
-        return (
-            <Paper className={classes.root}>
-                <Title>Prédios</Title>
-                <Table className={classes.table} aria-label="simple table">
-                    <TableHead>
-                        <TableRow>
-                            <TableCell>Nome</TableCell>
-                            <TableCell>Status</TableCell>
-                            <TableCell align="center">Ações</TableCell>
+        getBuildings();
+    }, []);
+
+    return (
+        <Paper className={classes.root}>
+            <Title>Prédios</Title>
+            <Table className={classes.table} aria-label="simple table">
+                <TableHead>
+                    <TableRow>
+                        <TableCell>Nome</TableCell>
+                        <TableCell>Status</TableCell>
+                        <TableCell align="center">Ações</TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {building.map((building, index) => (
+                        <TableRow key={index}>
+                            <TableCell component="th" scope="row">
+                                {building.name}
+                            </TableCell>
+                            <TableCell>{building.status ? "Ativo" : "Inativo"}</TableCell>
+                            <TableCell align="center">Botões</TableCell>
                         </TableRow>
-                    </TableHead>
-                    <TableBody>
-                        {building.map((building, index) => (
-                            <TableRow key={index}>
-                                <TableCell component="th" scope="row">
-                                    {building.name}
-                                </TableCell>
-                                <TableCell>{building.status}</TableCell>
-                                <TableCell align="center">Botões</TableCell>
-                            </TableRow>
-                        ))}
-                    </TableBody>
-                </Table>
-            </Paper>
-        )
-    };
+                    ))}
+                </TableBody>
+            </Table>
+        </Paper>
+    )
 }
