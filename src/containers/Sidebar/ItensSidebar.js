8641eec42b8fe6351c4689ce9b10e13aeaa63f7d
Henrique Rosa <henrique.rosa@meta.com.br>
2020-03-31 18:04:50 -0300

    add inicio tables

--- src/containers/Sidebar/ItensSidebar.js
+++ src/containers/Sidebar/ItensSidebar.js
@@ -3,7 +3,7 @@ import { Apartment as ApartmentIcon } from "@material-ui/icons";
 
 const Items = [
   { id: 0, label: "Dashboard", link: "/predios", icon: <ApartmentIcon /> },
-  { id: 0, label: "teste", link: "/teste", icon: <ApartmentIcon /> }
+  { id: 1, label: "teste", link: "/teste", icon: <ApartmentIcon /> }
 ];
 
 export default Items;
--- src/views/Building/Index.jsx
+++ src/views/Building/Index.jsx
@@ -1,5 +1,92 @@
 import React from "react";
+import MUIDataTable from "mui-datatables";
+import { Switch, FormControlLabel } from "@material-ui/core";
 
-const BuildingView = () => <h1>Prédios</h1>;
+const BuildingView = () => {
+  const columns = [
+    {
+      name: "id",
+      label: "id",
+      options: {
+        filter: false,
+        display: "excluded"
+      }
+    },
+    {
+      name: "name",
+      label: "Nome",
+      options: {
+        filter: true
+      }
+    },
+    {
+      name: "active",
+      label: "Ativo",
+      options: {
+        filter: true,
+        filterOptions: {
+          names: ["Ativo", "Inativo"]
+        },
+        customBodyRender: (value, tableMeta, updateValue) => {
+          return (
+            <FormControlLabel
+              label={value ? "Sim" : "Não"}
+              value={value}
+              control={<Switch color="primary" checked={value} value={value} />}
+              onChange={event => {
+                updateValue(event.target.value !== "true");
+              }}
+            />
+          );
+        }
+      }
+    }
+  ];
+
+  const data = [
+    {
+      name: "Prédio 1",
+      active: true
+    },
+    {
+      name: "Prédio 2",
+      active: true
+    },
+    {
+      name: "Prédio 3",
+      active: false
+    },
+    {
+      name: "Prédio 4",
+      active: true
+    }
+  ];
+
+  const options = {
+    selectableRows: "none",
+    print: false,
+    download: false,
+    responsive: "scrollMaxHeight",
+    filterType: "dropdown",
+    textLabels: {
+      body: {
+        noMatch: "Nenhum registro encontrado!"
+      },
+      filter: {
+        all: "Todos",
+        title: "Filtros",
+        reset: "Limpar Filtros"
+      }
+    }
+  };
+  return (
+    <MUIDataTable
+      title={"Prédios"}
+      data={data}
+      columns={columns}
+      options={options}
+    />
+  );
+};
 
 export default BuildingView;
